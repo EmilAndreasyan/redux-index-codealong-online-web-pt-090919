3b32373f216e16e2de24ad1ca840e217a40d9423
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-10 06:24:40 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -19,6 +19,7 @@ class TodosContainer extends Component {
 }
 
 const mapStateToProps = state => {
-  return {todos: state.todos}}
+  return {todos: state.todos}
+}
 
 export default connect(mapStateToProps)(TodosContainer);
